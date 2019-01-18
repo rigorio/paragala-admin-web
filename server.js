23914fcd747df58617e6b7a4853bc4a339e36c4f
rigorio <rigo.sarmiento@pm..me>
2019-01-18 23:46:45 +0800

    c1

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-var express = require('express'),
-  app = express();
+var express  = require('express');
+var app      = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-app.use(express.static('www'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(cors());
 
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// API Routes
-// app.get('/blah', routeHandler);
-
+app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
-
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
